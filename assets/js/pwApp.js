abb995b1e525061020a0ccd89df13b8299b7c1d7
Scott Kumor <scott.kumor1212@gmail.com>
2019-10-07 13:47:22 -0500

    naming fixes, no extra work was doen in this push

--- assets/js/pwApp.js
+++ assets/js/pwApp.js
@@ -31,7 +31,7 @@ function copyPass() {
 function genPass() {
   var newPass = [];
   var userPromptLength = document.getElementById('input').value;
-  var letterSetUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; 4
+  var letterSetUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var letterSetLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var numSet = ['1', '2', '3', '5', '6', '7', '8', '9', '0'];
   var specSet = ['!', '@', '$', '&', '~'];
